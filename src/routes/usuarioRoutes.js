243a86a1dab5f59bb0ac0037ab27514e63fd38a7
EveS0110 <evellysena550@gmail.com>
2025-12-09 10:24:33 -0300

    resolvendo conflito no arquivo usuarioRoutes.js

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import express from "express";
 const router = express.Router();
 
 // rota de teste temporária
 router.get("/", (req, res) => {
-  res.json({ message: "Rota de usuários funcionando! ✅" });
-=======
-import { Router } from 'express';
-const router = Router();
-
-// exemplo de rota
-router.get('/', (req, res) => {
-  res.send('Rota de usuários funcionando!');
->>>>>>> main
+  res.json({ message: "Rota de usuários funcionando!" });
 });
 
 export default router;
